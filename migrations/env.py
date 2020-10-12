5307b5dc12c25a274896e33c0a1bb4d57b8b32bc
CarlosCordoba96 <Carlos.Cordoba@alu.uclm.es>
2020-10-12 18:37:40 +0200

    Fix the import error when is executed out of the poetry shell

--- migrations/env.py
+++ migrations/env.py
@@ -4,7 +4,9 @@ from sqlalchemy import engine_from_config
 from sqlalchemy import pool
 
 from alembic import context
-
+import sys
+import os
+sys.path.append(os.getcwd())
 # this is the Alembic Config object, which provides
 # access to the values within the .ini file in use.
 config = context.config
